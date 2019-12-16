b15b696f03914745bd0291431efeecab6de8e1c1
massaman curry <m.m.elcapitan@gmail.com>
2019-12-16 10:11:23 +0900

    add abc083b file

---
+++ abc083b.php
@@ -0,0 +1,38 @@
+<?php
+
+fscanf(STDIN, "%d %d %d", $n, $a, $b);
+$result = 0;
+
+for($i = 1; $i <= $n; $i++){ //1,2,3...20
+
+    // $i_array = [];
+    $i_array = [];
+
+    // $i_array = str_split($i);
+
+    foreach(str_split($i) as $value){
+
+        $value = intval($value);
+        $i_array[] = $value;
+
+        
+
+        if(count($i_array) == strlen($i)){
+
+            $sum_array = array_sum($i_array);
+
+            if($a <= $sum_array && $sum_array <= $b){
+                $result += $i;
+            }
+
+        }
+    
+    }
+
+    // if($a <= $i_sum && $i_sum <= $b){
+    //     $result += $i_sum;
+    // }
+
+}
+
+print($result);
