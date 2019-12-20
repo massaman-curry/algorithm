de5f631d008271b7e00139b9a789f4c5c5116788
massaman curry <m.m.elcapitan@gmail.com>
2019-12-21 00:18:01 +0900

    add abc054 file

---
+++ abc054c.php
@@ -0,0 +1,35 @@
+<?php
+
+$arrays = [[1, 3], [2, 7], [3, 4], [4, 5], [4, 6], [5, 6], [6, 7]];
+$start = [];
+$search_point = 1;
+
+// function($input){
+
+    
+// }
+
+// for($i = 0; $i < count($arrays); $i++){
+
+//     if($arrays[$i][0] == $search_point){
+
+
+
+//     }
+
+// }
+
+
+
+foreach($arrays as $array){
+    [
+
+    foreach($array as $value){
+
+        $judge = ($value == $search_point) ?: FALSE;
+
+    }
+
+    $array[0] == $search_point ? $start[] = $array : '';
+
+}
