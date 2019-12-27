0d54c943a9c49f5e7795361b731335cc520fce2b
massaman curry <m.m.elcapitan@gmail.com>
2019-12-27 12:08:58 +0900

    modified abc057 with accepted method

--- abc057c.php
+++ abc057c.php
@@ -1,13 +1,24 @@
 <?php
 
 $n = trim(fgets(STDIN));
+$nsize = strlen(floor(sqrt($n)));
+$rslt = strlen($n);
 // $div_min = strlen($n);
 
 // $n = 1000;
 
-for($i = ceil(sqrt($n)); $i <= $n; $i++){
-    if($n%$i == 0) { print strlen($i) ."\n"; break;}
+for($i = 1; $i <= floor(sqrt($n));  $i++){
+    if($n%$i == 0){
+        $rslt = strlen($n/$i);
+        if($rslt == $nsize) break;
+    }
 }
+
+print $rslt;
+
+// for($i = ceil(sqrt($n)); $i <= $n; $i++){
+//     if($n%$i == 0) { print strlen($i) ."\n"; break;}
+// }
 
 // foreach(range(1, sqrt($n)) as $v){
 //     $v
