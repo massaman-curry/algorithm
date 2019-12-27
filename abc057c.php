f27cfb5234559b11f54b8fccb302f6ecaa7697a8
massaman curry <m.m.elcapitan@gmail.com>
2019-12-27 10:14:02 +0900

    update abc057c but exceed time limit

--- abc057c.php
+++ abc057c.php
@@ -1,13 +1,26 @@
 <?php
 
 $n = trim(fgets(STDIN));
-$div_min = strlen($n);
+// $div_min = strlen($n);
 
-for($i = 1; $i < $n/2; $i++){
+// $n = 1000;
 
-    if($n % $i == 0 && strlen(max($i, $n/$i)) < $div_min) $div_min = $i;
-
+for($i = ceil(sqrt($n)); $i <= $n; $i++){
+    if($n%$i == 0) { print strlen($i) ."\n"; break;}
 }
 
-print $div_min;
+// foreach(range(1, sqrt($n)) as $v){
+//     $v
+// }
+
+// for($i = 1; $i < $n/2; $i++){
+//     // if($n % $i == 0 && $len = strlen(max($i, $n/$i)) < $div_min) $div_min = $len;
+//     if($n % $i == 0){
+//         $len = strlen(max($i, $n/$i));
+//         $div_min = $len < $div_min ? $len : $div_min;
+//     }
+
+// }
+
+// print $div_min;
 
