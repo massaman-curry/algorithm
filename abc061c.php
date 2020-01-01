e8cbc0050c331f7d437ba06565c71ce8d9919bc9
massaman curry <m.m.elcapitan@gmail.com>
2020-01-01 17:00:19 +0900

    implement another method on abc061c to prevent from time limit error

--- abc061c.php
+++ abc061c.php
@@ -7,21 +7,46 @@
 
 // $n = 3;
 // $k = 4;
+
 fscanf(STDIN, "%d %d", $n, $k);
 $array = [];
+$cnt = 0;
 
-for($i = 0; $i < $n; $i++){
-
+while(count($array) < $k){
     fscanf(STDIN, "%d %d", $a, $b);
-
-    for($j = 0; $j < $b; $j++) $array[] = $a;
-
+    for($i = 0; $i < $b; $i++) $array[] = $a;
+    $cnt++;
 }
 
 usort($array, function($a, $b){
     return $a <=> $b;
 });
 
-// var_dump($array);
+for($i = 0; $i < $n - $cnt; $i++){
+    fscanf(STDIN, "%d %d", $a, $b);
+    if($a <= $array[$k -1]) continue;
+    for($i = 0; $i < $b; $i++) $array[] = $a;
+}
 
-print $array[$k -1];
+print $array[$k];
+
+
+
+// fscanf(STDIN, "%d %d", $n, $k);
+// $array = [];
+
+// for($i = 0; $i < $n; $i++){
+
+//     fscanf(STDIN, "%d %d", $a, $b);
+
+//     for($j = 0; $j < $b; $j++) $array[] = $a;
+
+// }
+
+// usort($array, function($a, $b){
+//     return $a <=> $b;
+// });
+
+// // var_dump($array);
+
+// print $array[$k -1];
