56c9aa305df1fe78ad7c50c1adba3a26bbe9d005
massaman curry <m.m.elcapitan@gmail.com>
2020-01-01 17:36:43 +0900

    add new method on abc061c but it has not tested yet

--- abc061c.php
+++ abc061c.php
@@ -12,23 +12,49 @@ fscanf(STDIN, "%d %d", $n, $k);
 $array = [];
 $cnt = 0;
 
-while(count($array) < $k){
-    fscanf(STDIN, "%d %d", $a, $b);
-    for($i = 0; $i < $b; $i++) $array[] = $a;
-    $cnt++;
+for($i = 0; $i < $n; $i++){
+    fscanf(STDIN,  "%d %d", $a, $b);
+    $array[$a] += $b;
 }
 
 usort($array, function($a, $b){
     return $a <=> $b;
 });
 
-for($i = 0; $i < $n - $cnt; $i++){
-    fscanf(STDIN, "%d %d", $a, $b);
-    if($a <= $array[$k -1]) continue;
-    for($i = 0; $i < $b; $i++) $array[] = $a;
+while(true){
+    $sum = 0;
+    foreach($array as $key => $value){
+        $sum += $value;
+    }
+
+    if($sum >= $k){}
+
+    //ここで、$arrayから$key => $valueを取り出して、$kを超えるまで$valueを足し、超えた瞬間の$keyを取出したい
 }
 
-print $array[$k];
+// foreach($array as $key => $value){
+//     for($i = 0; $i < $k;){
+
+//     }
+// }
+
+// while(count($array) < $k){
+//     fscanf(STDIN, "%d %d", $a, $b);
+//     for($i = 0; $i < $b; $i++) $array[] = $a;
+//     $cnt++;
+// }
+
+// usort($array, function($a, $b){
+//     return $a <=> $b;
+// });
+
+// for($i = 0; $i < $n - $cnt; $i++){
+//     fscanf(STDIN, "%d %d", $a, $b);
+//     if($a <= $array[$k -1]) continue;
+//     for($i = 0; $i < $b; $i++) $array[] = $a;
+// }
+
+// print $array[$k];
 
 
 
