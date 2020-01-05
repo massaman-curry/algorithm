029b36873c37b372ff788ba734f315fdb344f48c
massaman curry <m.m.elcapitan@gmail.com>
2020-01-05 23:40:04 +0900

    modified abc064 but does not work collectly

--- abc064c.php
+++ abc064c.php
@@ -4,8 +4,9 @@ $n = fgets(STDIN);
 $input = explode(" ", fgets(STDIN));
 
 // $colour = ['灰色', '茶色', '緑色', '水色', '青色', '黄色', '橙色', '赤色'];
-$cntr = array_fill(0, 12, 0);
+$cntr = array_fill(0, 13, 0);
 $rslt = 0;
+$max = 0;
 
 foreach($input as $v){
     $cntr[intval(floor((int)$v / 400))] += 1;
@@ -15,7 +16,13 @@ foreach($input as $v){
 
 for($i = 0; $i < 8; $i++) if($cntr[$i] > 0) $rslt += 1;
 
-for($i = 8; $i < 11; $i++) $cntr[11] += $cntr[$i];
+for($i = 8; $i < 12; $i++) $cntr[12] += $cntr[$i];
 
-echo $rslt, " ", $rslt + $cntr[11];
+$max = ($rslt + $cntr[12]) > 8 ? 8 : $rslt + $cntr[12];
+
+// $max = $rslt + $cntr[12];
+
+// if($rslt + $cntr[12] > 8) $max
+
+echo $rslt, " ", $max;
 
