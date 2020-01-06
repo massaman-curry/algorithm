9ea21af4d1b6101daf7fab684cd3de513fccaf59
massaman curry <m.m.elcapitan@gmail.com>
2020-01-06 12:45:23 +0900

    rollback abc064c to former version

--- abc064c.php
+++ abc064c.php
@@ -9,16 +9,19 @@ $rslt = 0;
 $max = 0;
 
 foreach($input as $v){
-    $cntr[intval(floor((int)$v / 400))] += 1;
+    $cntr[intval(floor((int)$v / 400))] = 1;
 }
 
 // var_dump($cntr);
 
-for($i = 0; $i < 8; $i++) if($cntr[$i] > 0) $rslt += 1;
-
+for($i = 0; $i < 8; $i++) $rslt += $cntr[$i];
 for($i = 8; $i < 12; $i++) $cntr[12] += $cntr[$i];
 
-$max = ($rslt + $cntr[12]) > 8 ? 8 : $rslt + $cntr[12];
+
+
+$max = $rslt + $cntr[12];
+
+// $max = ($rslt + $cntr[12]) > 8 ? 8 : $rslt + $cntr[12];
 
 // $max = $rslt + $cntr[12];
 
