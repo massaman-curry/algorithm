e49dcfd4b9c3376c85a441fe50d0ecac9ab36946
massaman_curry <m.m.elcapitan@gmail.com>
2020-01-17 21:35:31 +0900

    unaccepted and decided to quit

--- abc076c.php
+++ abc076c.php
@@ -3,28 +3,50 @@
 //s =  ?tc????
 // t = coder
 
-$s = ['?', 't', 'c', '?', '?', '?', '?'];
-$t = ['c', 'o', 'd', 'e', 'r'];
-$rslt;
+$s = str_split(fgets(STDIN));
+$t = str_split(fgets(STDIN));
+
+// $s = ['?', 't', 'c', '?', '?', '?', '?'];
+// $t = ['c', 'o', 'd', 'e', 'r'];
+$rslt = 0;
 $ans = 'UNRESTORABLE';
 
 for($i = 0; $i < count($s) - count($t) +1; $i++){
+    $cnt = 0;
     for($j = 0; $j < count($t); $j++){
         // echo $t[$j], $s[$i + $j];
-        if($t[$j] != $s[$i + $j]  && $s[$i + $j] != '?') echo $t[$j], $s[$i + $j];
+        if($t[$j] != $s[$i + $j]  && $s[$i + $j] != '?'){
+            // echo $t[$j], $s[$i + $j];
+        break;
+        }
 
+        $cnt++;
+
+        // $rslt = true;
+
+        // print "hello";
         // if($t[$j] != $s[$i + $j] && $s[$j] != '?') echo $t[$j], $s[$i+$j];
         // echo $i, $j;
         // echo $s[$i], $t[$j];
         // if($t[$j] != $s[$i+$j] && $s[$i+$j] != '?') {$rslt = false; break;}
     }
+
+    if($cnt == count($t)) $rslt = $i;
 }
+
+// echo $rslt;
 
 if($rslt){
     for($i = 0; $i < count($t); $i++){
         $s[$rslt + $i] = $t[$i];
     }
-    foreach($s as $v) $v = $v == '?' ? 'a' : $v;
+    
+    for($i = 0; $i < count($t); $i++){
+        if($s[$i] == '?') $s[$i] = "a";
+    }
+
+    // var_dump($s);
+    // $v = $v == '?' ? 'a' : $v;
     $ans = implode('', $s);
 }
 
