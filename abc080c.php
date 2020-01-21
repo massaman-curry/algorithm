90862d1164c7cf87d50c334de7c43a290527f786
massaman_curry <m.m.elcapitan@gmail.com>
2020-01-21 23:41:59 +0900

    modified abc080c and changed its algorithm to dfs

--- abc080c.php
+++ abc080c.php
@@ -1,63 +1,91 @@
 <?php
 
-// 2
-// 1 1 1 1 1 0 0 0 0 0
-// 0 0 0 0 0 1 1 1 1 1
-// 0 -2 -2 -2 -2 -2 -1 -1 -1 -1 -1
-// 0 -2 -2 -2 -2 -2 -1 -1 -1 -1 -1
+$f = [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+];
+$p = [[0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1],
+      [0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1]
+];
 
-// $f = [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-//       [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
-// ];
-// $p = [[0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1],
-//       [0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1]
-// ];
+$a = [];
 
-$f = [];
-$p = [];
-
-fscanf(STDIN, "%d", $n);
-for($i = 0; $i < $n; $i++){
-    $f[] = array_map('intval', explode(' ', fgets(STDIN)));
-}
-
-for($i = 0; $i < $n; $i++){
-    $p[] = array_map('intval', explode(' ', fgets(STDIN)));
-}
-
-// var_dump($f);
-// var_dump($p);
-
-$sum = [0];
-
-// echo $p[0][1];
-//下の2は、$nに変える
-for($i = 0; $i < $n; $i++){
-    $cnt = 0;
-    uasort($p[$i], function($b, $a){
-        return $a <=> $b;
-    });
-    // var_dump($p[$i]);
-    foreach($f[$i] as $v){
-        // var_dump($v) ;
-        // if($v == 1) $cnt+= 1;
-        $cnt += $v;
-    }
-
-    // echo $cnt;
-
-    // echo $cnt;
-    // $search = array_keys($p[$i], max($p[$i]));
-    // var_dump($search);
-    foreach($p[$i] as $k => $v){
-        if($cnt >= $k && $k != 0) {
-            // echo $p[$i][$k];
-            $sum[$i + 1] = $sum[$i] + $p[$i][$k];
-        break;
+function score(){
+    $rslt = 0;
+    for($i = 0; $i < $n; $i++){
+        $cnt = 0;
+        for($j = 0; $j < count($f[$i]); $j++){
+            if($f[$i][$j] == $a[$j]) $cnt++;
+            $rslt = $p[$i][$cnt];
         }
-        // if($f[$i][$k]){$sum += $p[$i][$k]; break;}
     }
+    return $rslt;
 }
 
-$sum[0] = null;
-echo max($sum);
+function dfs($pos){
+    global $a;
+    if($pos == 10) score(); return;
+    $a[$pos] = 0; dfs($pos + 1);
+    $a[$pos] = 1; dfs($pos + 1);
+}
+
+// // 2
+// // 1 1 1 1 1 0 0 0 0 0
+// // 0 0 0 0 0 1 1 1 1 1
+// // 0 -2 -2 -2 -2 -2 -1 -1 -1 -1 -1
+// // 0 -2 -2 -2 -2 -2 -1 -1 -1 -1 -1
+
+// // $f = [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+// //       [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+// // ];
+// // $p = [[0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1],
+// //       [0, -2, -2, -2, -2, -2, -1, -1, -1, -1, -1]
+// // ];
+
+// $f = [];
+// $p = [];
+
+// fscanf(STDIN, "%d", $n);
+// for($i = 0; $i < $n; $i++){
+//     $f[] = array_map('intval', explode(' ', fgets(STDIN)));
+// }
+
+// for($i = 0; $i < $n; $i++){
+//     $p[] = array_map('intval', explode(' ', fgets(STDIN)));
+// }
+
+// // var_dump($f);
+// // var_dump($p);
+
+// $sum = [0];
+
+// // echo $p[0][1];
+// //下の2は、$nに変える
+// for($i = 0; $i < $n; $i++){
+//     $cnt = 0;
+//     uasort($p[$i], function($b, $a){
+//         return $a <=> $b;
+//     });
+//     // var_dump($p[$i]);
+//     foreach($f[$i] as $v){
+//         // var_dump($v) ;
+//         // if($v == 1) $cnt+= 1;
+//         $cnt += $v;
+//     }
+
+//     // echo $cnt;
+
+//     // echo $cnt;
+//     // $search = array_keys($p[$i], max($p[$i]));
+//     // var_dump($search);
+//     foreach($p[$i] as $k => $v){
+//         if($cnt >= $k && $k != 0) {
+//             // echo $p[$i][$k];
+//             $sum[$i + 1] = $sum[$i] + $p[$i][$k];
+//         break;
+//         }
+//         // if($f[$i][$k]){$sum += $p[$i][$k]; break;}
+//     }
+// }
+
+// $sum[0] = null;
+// echo max($sum);
