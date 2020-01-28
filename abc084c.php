32876840b8751c17efb3610553fce9803eecd204
massaman_curry <m.m.elcapitan@gmail.com>
2020-01-28 23:49:27 +0900

    modified abc084c with error code

--- abc084c.php
+++ abc084c.php
@@ -10,15 +10,23 @@ for($i = 0; $i < $n - 1; $i++){
 }
 // var_dump($in);
 
-for($i = 0; $i < $n - 1; $i++){
+for($i = 0; $i < $n-1; $i++){
     $sum = $in[$i][1];
-    for($j = $i; $j < $n - 1; $j++){
-        
+    for($j = $i; $j < $n-2; $j++){
+        if($sum + $in[$j][0] > $in[$j+1][1]){
+            $sum += $in[$j][0];
+            $sum += ($sum - $in[$j][1]) % $in[$j][2];
+            continue;
+        }
+        $sum = $in[$j+1][1];
+    }
+    $sum += $in[$n-1][0];
+
+    echo $sum;
+}
+echo 0;
+
         // if($sum < $in[$n-2][1]){
         //     $sum = $in[$n-2][0] + $in[$n-2][1];
         //     continue;
         // }
-        $sum += $in[$j][0];
-    }
-    echo $sum;
-}
