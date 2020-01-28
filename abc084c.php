9d7fbb41176e8de03b8e8382d0d9e9e570600a5f
massaman_curry <m.m.elcapitan@gmail.com>
2020-01-28 11:26:48 +0900

    modified bac084c and add function to calculate but not accepted yet

--- abc084c.php
+++ abc084c.php
@@ -4,10 +4,21 @@
 // 44 17 17
 // 66 4096 64
 $in = [];
-
 fscanf(STDIN, "%d", $n);
 for($i = 0; $i < $n - 1; $i++){
-    $in[] = explode(' ', fgets(STDIN));
+    $in[] = array_map('intval', explode(' ', fgets(STDIN)));
 }
+// var_dump($in);
 
-var_dump($in);
+for($i = 0; $i < $n - 1; $i++){
+    $sum = $in[$i][1];
+    for($j = $i; $j < $n - 1; $j++){
+        
+        // if($sum < $in[$n-2][1]){
+        //     $sum = $in[$n-2][0] + $in[$n-2][1];
+        //     continue;
+        // }
+        $sum += $in[$j][0];
+    }
+    echo $sum;
+}
