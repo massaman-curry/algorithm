6998dce003a0ec13e391c708a46bab514098ed6e
massaman_curry <m.m.elcapitan@gmail.com>
2020-01-30 16:36:28 +0900

    modified abc085c2 but it requires too much time

--- abc085c2.php
+++ abc085c2.php
@@ -3,16 +3,27 @@
 fscanf(STDIN, "%d %d", $n, $y);
 
 $com = [];
+$ans = [-1, -1, -1];
 //x, y, z(10000. 5000, 1000)
+
+function sum($com){
+    global $n;
+    global $y;
+    if((array_sum($com) == $n) && (10000 * $com[0] + 5000 * $com[1] + 1000 * $com[2] == $y)){
+        foreach($com as $v) echo $v . ' ';
+        exit;
+    }
+}
 
 function dfs($pos){
     global $com;
     global $n;
 
     if($pos == 3){
-        print_r($com);
+        sum($com);
         return;
     };
+
     for($i = 0; $i <= $n; $i++){
         $com[$pos] = $i;
         dfs($pos + 1);
@@ -20,3 +31,4 @@ function dfs($pos){
 }
 
 dfs(0);
+echo -1, ' ', -1, ' ', -1;
