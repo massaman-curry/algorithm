0623ae2f45d5f57f2ed74f86a80774bfe8497a06
massaman curry <m.m.elcapitan@gmail.com>
2020-02-04 11:26:03 +0900

    update abc099c to bring file

--- abc099c.php
+++ abc099c.php
@@ -1,1 +1,13 @@
 <?php
+
+$dp = [];
+$dp[0] = 0;
+
+for($n = 1; $n <= 100000; $n++){
+    $dp[$n] = PHP_INT_MAX;
+    $power = 1;
+    while($power <= $n){
+        $dp[$n] = min($dp[$n], $dp[$n - $power] + 1);
+        $power *= 6;
+    }
+}
