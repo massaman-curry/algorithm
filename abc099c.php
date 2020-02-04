b755feee031f73a4acc2f24e306c46d76c353def
massaman_curry <m.m.elcapitan@gmail.com>
2020-02-04 22:24:06 +0900

    modified abc099c and accepted

--- abc099c.php
+++ abc099c.php
@@ -9,5 +9,14 @@ for($n = 1; $n <= 100000; $n++){
     while($power <= $n){
         $dp[$n] = min($dp[$n], $dp[$n - $power] + 1);
         $power *= 6;
+        //1 + $dp[$n - 6], 1 + $dp[$n - 36]...
+    }
+    $power = 1;
+    while($power <= $n){
+        $dp[$n] = min($dp[$n], $dp[$n - $power] + 1);
+        $power *= 9;
     }
 }
+
+fscanf(STDIN, "%d", $N);
+echo $dp[$N];
